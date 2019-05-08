13e3be51f14d0ce8b0bf8e52509b7e7183a42022
Pocket of Weird <hartzlernathan@gmail.com>
2019-05-08 13:49:05 -0500

    Working out bugs

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { withFirebase, isLoaded, isEmpty } from 'react-redux-firebase';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import Home from './components/Home';
+import Loading from './components/Loading';
 import Login from './components/Login';
+import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
 
 
-const Routes = () => (
+const Routes = (props: any) => (
   <Router>
     <Route exact path="/" component={Home} />
-    <Route path="/login" component={Login} />
-    <Route path="/app" component={Dashboard} />
+    {
+      !isLoaded(props.auth) ?
+        <Loading />
+      : isEmpty(props.auth) ?
+        <React.Fragment>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </React.Fragment>
+      :
+        <React.Fragment>
+          <Route path="/app" component={Dashboard} />
+        </React.Fragment>
+    }
   </Router>
 )
 
-export default Routes;
+export default compose<any>(
+  withFirebase,
+  connect(({ firebase: { auth } }: any) => ({ auth }))
+)(Routes);
---
+++ src/components/Signup.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Signup = (props: object) => (
+    <React.Fragment>
+      <h1>Signup</h1>
+    </React.Fragment>
+)
+
+export default Signup;
