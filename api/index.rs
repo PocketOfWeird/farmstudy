7e9a542df32bb2ca9e3cda19e50bf3ff9baf6328
Hartzler <ftn0813@missouristate.edu>
2019-05-17 15:06:50 -0500

    Further testing

--- api/index.rs
+++ api/index.rs
@@ -1,10 +1,26 @@
 use http::{Request, Response, StatusCode, header};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use serde_json::{Value};
+
+
+#[derive(Serialize, Deserialize)]
+struct Crop {
+    name: String,
+    color: String,
+}
 
 fn handler(request: Request<()>) -> http::Result<Response<String>> {
+
+    let crop = Crop {
+        name: "Broccoli".to_owned(),
+        color: "Usually Green".to_own(),
+    }
+
     let response = Response::builder()
         .status(StatusCode::OK)
-        .header(header::CONTENT_TYPE, "text/html")
-        .body("<!doctype html><html><head><title>A simple deployment with Now!</title></head><body><h1>Welcome to Rust on Now</h1></body></html>".to_string())
+        .header(header::CONTENT_TYPE, "text/json")
+        .body(serde_json::to_string(&crop)?)
         .expect("failed to render response");
 
     return Ok(response);
